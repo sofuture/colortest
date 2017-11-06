8fbb9f58a732a4c133d21cfb595c9eb438859718
Jeff Zellner <jeffz@eml.cc>
2017-11-05 18:29:54 -0700

    vendor my fork of github.com/labstack/echo

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/fatih/color"
+	"github.com/labstack/echo"
 )
 
 func main() {
 	color.White("Is this white?")
 	color.Black("Is this black?")
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
